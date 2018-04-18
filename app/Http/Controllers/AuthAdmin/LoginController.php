f3b8ba010f348ebf1a2c61f2990f71fbb5f4fe7a
krisnamr <krisna_mustika@ymail.com>
2018-04-18 09:48:37 +0700

    uot

---
+++ app/Http/Controllers/AuthAdmin/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\AuthAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except(['logout']);
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('authAdmin.login');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        // Attempt to log the user in
+        if (Auth::guard('admin')->attempt($credential, $request->remember)){
+            // If login succesful, then redirect to their intended location
+            return redirect()->intended(route('admin.home'));
+        }
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+}
